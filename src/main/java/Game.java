ba8bf4c05b1be0784458fde7cabb290825c141fa
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2023-12-02 00:08:51 +0700

    HashMap is used

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,40 +1,44 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
-    ArrayList<Player> players = new ArrayList<>();
+    //ArrayList<Player> players = new ArrayList<>();
+    HashMap<String, Integer> players = new HashMap<>();
 
-    public void register(Player player) {
-        players.add(player);
+    public void register(String playerName, int strength) {
+        players.put(playerName, strength);
     }
 
     public int round(String playerName1, String playerName2) {
 
         int result = 0;
-        Player player1 = null;
-        Player player2 = null;
+        int strength1 = 0;
+        int strength2 = 0;
 
-        for (Player player : players) {
-            if (player.getName() == playerName1) {
-                player1 = player;
-            }
-            if (player.getName() == playerName2) {
-                player2 = player;
-            }
-        }
-
-        if (player1 == null) {
+        if (players.get(playerName1) == null) {
             throw new NotRegisteredException("Нету такого игрока");
         }
-        if (player2 == null) {
+
+        if (players.get(playerName2) == null) {
             throw new NotRegisteredException("Нету такого игрока");
         }
-        if (player1.getStrength() > player2.getStrength()) {
-            result = 1;
-        }
-        if (player1.getStrength() < player2.getStrength()) {
-            result = 2;
+
+        for (String key : players.keySet()) {
+            if (key == playerName1) {
+                strength1 = players.get(key);
+            }
+            if (key == playerName2) {
+                strength2 = players.get(key);
+            }
         }
 
-        return result;
+            if (strength1 > strength2) {
+                result = 1;
+            }
+            if (strength1 < strength2) {
+                result = 2;
+            }
+            return result;
+        }
     }
-}
+
